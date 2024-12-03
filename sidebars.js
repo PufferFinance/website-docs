55d15e97f34169c440935af241d118f5f268b7c4
Xanik <sandraike57@gmail.com>
2024-12-03 13:43:17 +0000

    feat: added ejection scenario md to node doc

--- sidebars.js
+++ sidebars.js
@@ -42,7 +42,7 @@ const sidebars = {
     {
       type: "category",
       label: "Node Operators",
-      items: ["requirements", "setup", "registration", "noop-rewards"],
+      items: ["requirements", "setup", "registration", "noop-rewards", "ejection-scenarios"],
     },
     {
       type: "category",
